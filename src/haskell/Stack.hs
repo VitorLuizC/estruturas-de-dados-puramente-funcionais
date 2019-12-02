ce7a9ecf53c13699b29fe50b67329f9a49154797
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-12-02 00:15:53 -0300

    :zap: conserta com os erros de compilação no Stack.hs

--- src/haskell/Stack.hs
+++ src/haskell/Stack.hs
@@ -1,7 +1,5 @@
 module Stack where
 
-import Data.Maybe
-
 data Stack a = Empty | Cons a (Stack a)
 
 -- Inicializa uma pilha com o valor para pilhas vazias.
@@ -10,12 +8,12 @@ init = Empty
 
 -- Obtém a cabeça da pilha, o primeiro elemento.
 head :: Stack a -> Maybe a
-head Empty = Nothing
+head Empty      = Nothing
 head (Cons x _) = Just x
 
 -- Obtém a cauda da pilha, todos os elementos que não o primeiro.
 tail :: Stack a -> Maybe (Stack a)
-tail Empty = Nothing
+tail Empty       = Nothing
 tail (Cons _ xs) = Just xs
 
 -- Checa se a pilha está vazia.
@@ -26,12 +24,12 @@ isEmpty _     = False
 -- Remove o elemento da Stack.
 -- Retorna numa tupla o primeiro elemento e uma pilha com os outros elementos.
 pop :: Stack a -> (Maybe a, Maybe (Stack a))
-pop stack = (head stack, tail stack)
+pop stack = (Stack.head stack, Stack.tail stack)
 
 -- Adiciona um elemento na Stack.
 push :: Stack a -> a -> Stack a
-push = Cons
+push stack value = Cons value stack
 
 -- Obtém o primeiro elemento da Stack.
 peek :: Stack a -> Maybe a
-peek = head
+peek = Stack.head
